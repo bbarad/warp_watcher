4ba8b302abe05feec1f4a7a189955cadbe3a42c0
Benjamin Barad <barad.benjamin@gene.com>
2019-09-03 15:47:48 -0700

    docstrings

--- main.py
+++ main.py
@@ -41,44 +41,55 @@ define('reload_rate', default=120000, help="How often to reload thumbnails, in m
 define('parent_path', type=str, help="Parent directory for warp jobs")
 
 # Set up globals for use throughout the program
-body_html = ""
 static_path = {"path": "/tmp/thumbnails"}
 warp_path = ["/tmp"]
+body_html = [f"Waiting to Load Micrographs - {warp_path[0]}"]
 settings = {"websocket_ping_interval": 30}
 clients = set()
 loader = tornado.template.Loader(".")
 string_loader = loader.load("template.html")
 options.parse_command_line()
 
-async def change_warp_path(new_path):
-    if not os.path.exists(os.path.join(options.parent_path, new_path, "thumbnails")):
-        print("No Thumbnail Directory Found")
+async def change_warp_path(new_directory):
+    """
+    Change to a different warp output folder
+    Args:
+        new_directory(str): directory name for new warp folder to watch
+    Returns:
+        bool: true if successful change of folder.
+    """
+    if not os.path.exists(os.path.join(options.parent_path, new_directory, "thumbnails")):
+        print("No Thumbnail Directory Found - Aborting")
         return False
     else:
-        warp_path[0] = os.path.join(options.parent_path, new_path)
-        static_path["path"] = os.path.join(options.parent_path, new_path, "thumbnails")
+        warp_path[0] = os.path.join(options.parent_path, new_directory)
+        static_path["path"] = os.path.join(options.parent_path, new_directory, "thumbnails")
         print(f"New Warp Directory: {warp_path[0]}")
     return True
 
 async def update_html_string(path, linecount=200):
+    """
+    Generate HTML for the gallery of last n classes. Stores in a global variable to minimize
+
+    Args:
+        path (str): Path to warp folder
+        linecount (int): Number of thumbnails to display
+    """
     thumbnail_dir = os.path.join(path, "thumbnails")
     if not os.path.exists(thumbnail_dir):
         await message_all_clients({"type": "alert", "data": "Could not find thumbnails in the chosen directory"})
         return
     files = [file for file in os.listdir(thumbnail_dir) if (file.lower().endswith('.png'))]
-    files.sort(key=lambda x: os.path.getctime(os.path.join(thumbnail_dir, x)), reverse=False)
+    files.sort(key=lambda x: os.path.getctime(os.path.join(thumbnail_dir, x)), reverse=True)
     total_count = len(files)
     if linecount > total_count:
         linecount = total_count
     return_list = [{"number": len(files)-i,"timestamp": datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(thumbnail_dir, j))).strftime('%Y-%m-%d %H:%M:%S'), "name": j[:-4], "url": os.path.join("/gallery/", j)} for i,j in enumerate(files[:linecount])]
     warp_name = os.path.split(path)[1]
 
-    body_html = string_loader.generate(thumbnail_list = return_list, warp_name = warp_name, line_count = linecount, total_count = total_count).decode("utf-8")
+    body_html[0] = string_loader.generate(thumbnail_list = return_list, warp_name = warp_name, line_count = linecount, total_count = total_count).decode("utf-8")
     print("Updating Client HTML")
-    await message_all_clients({"type": "gallery", "data": body_html})
-
-async def get_body_html():
-    return body_html
+    await message_all_clients({"type": "gallery", "data": body_html[0]})
 
 async def message_all_clients(message, clients = clients):
     """
@@ -97,7 +108,7 @@ async def message_all_clients(message, clients = clients):
 
 
 class IndexHandler(RequestHandler):
-    """Core class to respond to new clients."""
+    """Core class to respond to new clients. Extends :py:class:`tornado.web.RequestHandler`"""
     def get(self):
         """Minimal handler for setting up the very first connection via an HTTP request before setting up the websocket connection for all future interactions."""
         self.render("index.html")
@@ -112,6 +123,11 @@ class SocketHandler(WebSocketHandler):
         print("Socket Opened from {}".format(self.request.remote_ip))
 
     async def on_message(self, message):
+        """Handles messages from clients
+
+        Args:
+            message (str): string encoded json from the client. Should be of the format ``{"command": $COMMAND, "data": $DATA}``
+        """
         message_json = json.loads(message)
         command = message_json['command']
         data = message_json['data']
@@ -123,7 +139,8 @@ class SocketHandler(WebSocketHandler):
                 self.write_message({"type": "alert", "data": "Couldn't find a thumbnails folder in that directory - has Warp run there?"})
         elif command == "initialize":
             print("initializing with body html:")
-            await update_html_string(warp_path[0], linecount=options.thumbnail_count)
+            self.write_message({"type": "gallery", "data": body_html[0]})
+            # await update_html_string(warp_path[0], linecount=options.thumbnail_count)
         else:
             self.write_message({"type": "alert", "data": "Didn't understand that message"})
     def on_close(self):
